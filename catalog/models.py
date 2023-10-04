dcd72f333a4fe9e1c55da02f728383c477a1be08
Anastasia Chernysheva <nastya770098@gmail.com>
2023-10-04 23:57:54 +0300

    commit 2

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='')
+    description = models.CharField(max_length=300, verbose_name='')
+    image = models.ImageField(upload_to='products/', verbose_name='', **NULLABLE)  # необязательное поле
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    price = models.IntegerField
+    date_of_creation = models.DateField
+    date_of_change = models.DateField
+
+    def __str__(self):
+        return f'{self.title}, {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('title',)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    created_at = models.DateField
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import contacts, homepage
 
 urlpatterns = [
-    path('', contacts),
-    path('', homepage),
+                  path('contacts/', contacts),
+                  path('', homepage),
 
-]
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # тоже что-то для картинок
