2f283e17f52dff914991689d646455629f2b45e8
Anastasia Chernysheva <nastya770098@gmail.com>
2023-10-05 13:30:08 +0300

    commit 3 !

--- catalog/models.py
+++ catalog/models.py
@@ -9,9 +9,9 @@ class Product(models.Model):
     description = models.CharField(max_length=300, verbose_name='')
     image = models.ImageField(upload_to='products/', verbose_name='', **NULLABLE)  # необязательное поле
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    price = models.IntegerField
-    date_of_creation = models.DateField
-    date_of_change = models.DateField
+    price = models.IntegerField()
+    date_of_creation = models.DateField()
+    date_of_change = models.DateField()
 
     def __str__(self):
         return f'{self.title}, {self.price}'
@@ -25,5 +25,5 @@ class Product(models.Model):
 class Category(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
-    created_at = models.DateField
+
 
