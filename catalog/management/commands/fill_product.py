a56c64c709b1d1c897a056fa544295915dbfed66
Anastasia Chernysheva <nastya770098@gmail.com>
2023-10-09 13:20:07 +0300

    commit 5

---
+++ catalog/management/commands/fill_product.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+
+from catalog.models import Product
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        product_list = [
+            {'title': 'хлеб', 'description': 'свежий хлеб', 'category': 'еда', 'price': '60', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
+            {'title': 'майка', 'description': 'красивая майка', 'category': 'одежда', 'price': '1000', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
+            {'title': 'маленький принц', 'description': 'интересная книга', 'category': 'книги', 'price': '600', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
+            {'title': 'тушь', 'description': 'черная тушь', 'category': 'косметика', 'price': '1000', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
+        ]
+
+        product_to_create = []
+        for product_items in product_list:
+            product_to_create.append(
+                Product(**product_items)
+            )
+
+        Product.objects.bulk_create(product_to_create)
