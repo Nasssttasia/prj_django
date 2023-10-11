d4bcc70105862db1ea7d6151c694788f99d9e468
Anastasia Chernysheva <nastya770098@gmail.com>
2023-10-11 13:23:11 +0300

    commit 6

--- catalog/management/commands/fill_product.py
+++ catalog/management/commands/fill_product.py
@@ -5,18 +5,22 @@ from catalog.models import Product, Category
 
 class Command(BaseCommand):
 
-    category = Category.objects.all()
 
     def handle(self, *args, **options):
-        product_list = [
+
+        product = [
             {'title': 'хлеб', 'description': 'свежий хлеб', 'category': 'еда', 'price': '60', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
             {'title': 'майка', 'description': 'красивая майка', 'category': 'одежда', 'price': '1000', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
             {'title': 'маленький принц', 'description': 'интересная книга', 'category': 'книги', 'price': '600', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
             {'title': 'тушь', 'description': 'черная тушь', 'category': 'косметика', 'price': '1000', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
         ]
 
+        category_title = product.get('category')
+        category = Category.objects.get(title=category_title)
+        product = product['category'] = category
+
         product_to_create = []
-        for product_items in product_list:
+        for product_items in product:
             product_to_create.append(
                 Product(**product_items)
             )
