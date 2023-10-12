f842931ff985eb8e1042bfe7a88f90df13f55709
Anastasia Chernysheva <nastya770098@gmail.com>
2023-10-12 12:23:14 +0300

    commit 7

--- catalog/management/commands/fill_product.py
+++ catalog/management/commands/fill_product.py
@@ -1,5 +1,5 @@
 from django.core.management import BaseCommand
-
+from datetime import datetime
 from catalog.models import Product, Category
 
 
@@ -8,21 +8,33 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        product = [
+        products = [
             {'title': 'хлеб', 'description': 'свежий хлеб', 'category': 'еда', 'price': '60', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
             {'title': 'майка', 'description': 'красивая майка', 'category': 'одежда', 'price': '1000', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
             {'title': 'маленький принц', 'description': 'интересная книга', 'category': 'книги', 'price': '600', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
             {'title': 'тушь', 'description': 'черная тушь', 'category': 'косметика', 'price': '1000', 'date_of_creation': '09.10.2023', 'date_of_change': '09.10.2023'},
         ]
 
-        category_title = product.get('category')
-        category = Category.objects.get(title=category_title)
-        product = product['category'] = category
-
         product_to_create = []
-        for product_items in product:
+
+        # Перебираем продукты для изменения
+        for product in products:
+            # Получаем категорию
+            category_title = product.get('category')
+            category = Category.objects.get(title=category_title)
+
+            # Изменяем категорию продукта на значение из базы данных
+            product = product['category'] = category
+
+            # Приводим дату к виду YYYY-MM-DD
+            product['date_of_change'] = datetime.strptime(product['date_of_change'], '%d.%m.%Y').strftime('%Y-%m-%d')
+            product['date_of_creation'] = datetime.strptime(product['date_of_creation'], '%d.%m.%Y').strftime('%Y-%m-%d')
+
+            # Добавляем продукты в новый список
             product_to_create.append(
-                Product(**product_items)
+                Product(**product)
             )
+
+
 
         Product.objects.bulk_create(product_to_create)
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from catalog.apps import CatalogConfig
 from catalog.views import contacts, homepage, product
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 app_name = CatalogConfig.name
 
@@ -10,4 +13,4 @@ urlpatterns = [
                   path('', homepage, name='homepage'),
                   path('<int:pk>/product/', product, name='product'),
 
-              ]
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # тоже что-то для картинок
